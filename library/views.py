4fba19deae22e230037b0c0bcba74afdb65cc8e3
Philip Yao Aidam <phlpaidam@gmail.com>
2024-10-20 23:51:12 +0000

    Add features

--- library/views.py
+++ library/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Book, LibraryUser, Transaction
+from .serializers import BookSerializer, LibraryUserSerializer, TransactionSerializer
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class LibraryUserViewSet(viewsets.ModelViewSet):
+    queryset = LibraryUser.objects.all()
+    serializer_class = LibraryUserSerializer
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
