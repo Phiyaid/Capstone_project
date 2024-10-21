a770b94d320377ae6bf99387a4ce4a4a7a757742
Philip Yao Aidam <phlpaidam@gmail.com>
2024-10-21 18:08:22 +0000

    Update Library User Serializer

--- library/serializers.py
+++ library/serializers.py
@@ -6,12 +6,27 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
-class LibraryUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LibraryUser
-        fields = '__all__'
+# class LibraryUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = LibraryUser
+#         fields = '__all__'
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
+        
+        
+        
+class LibraryUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LibraryUser
+        fields = ['id', 'username', 'email', 'password', 'is_active']
+
+    def create(self, validated_data):
+        user = LibraryUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
